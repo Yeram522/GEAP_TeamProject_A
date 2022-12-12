7ec1576257ad0af9ddcf6fc237c274e83781e77e
이수진 <selina0@naver.com>
2022-12-13 06:19:52 +0900

    UI 끝
    라바 보스 = 스페이스바 누르면 체력 감소
    
    할로윈 보스 = 마우스 휠 누르면 체력 감소

---
+++ TeamProject_MainBranch/Assets/07_Scripts/realLAVABOSS/LAVAUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LAVAUI : MonoBehaviour
+{
+    Slider slHP;
+    float fSliderBarTime;
+    public float hpLAVA;
+    public GameObject gameObject;
+    void Start()
+    {
+        slHP = GetComponent<Slider>();
+    }
+
+
+    void Update()
+    {
+        if (slHP.value <= 0)
+            transform.Find("Fill Area").gameObject.SetActive(false);
+        else
+            transform.Find("Fill Area").gameObject.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.Space))// 스페이스바 누를 때
+        {
+            slHP.value -= hpLAVA;
+        }
+
+        if (slHP.value <= 0.001)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
